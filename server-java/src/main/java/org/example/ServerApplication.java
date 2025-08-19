1e1aabac0a6af487ebfef9a70c2f6e04c5cca4f9
mengfeijin <jin.xzy@outlook.com>
2025-08-19 16:22:48 -0700

    Initiate ServerApplication.java.

---
+++ server-java/src/main/java/org/example/ServerApplication.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+    //Start SpringBoot.
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+        System.out.println("Spring Boot Application Started.");
+    }
+
+}
